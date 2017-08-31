ef4473e60e7aca1b24b8c98c3ae36cd73d31bc55
skylu <skylu@msn.com>
2017-08-31 17:05:29 +0800

    fix crash by del remove handles code

--- janus.c
+++ janus.c
@@ -427,26 +427,7 @@ static gboolean janus_check_sessions(gpointer user_data) {
 			gint64 now = janus_get_monotonic_time();
 			if (now - session->last_activity >= (gint64)session_timeout * G_USEC_PER_SEC && !session->timeout) {
 				JANUS_LOG(LOG_INFO, "Timeout expired for session %"SCNu64"...\n", session->session_id);
-				/* Mark the session as over, we'll deal with it later */
-				janus_mutex_lock(&session->mutex);
-				session->timeout = 1;
-				/* Remove all handles */
-				/* 
-				if(session->ice_handles != NULL && g_hash_table_size(session->ice_handles) > 0) {
-					GHashTableIter iter;
-					gpointer value;
-					g_hash_table_iter_init(&iter, session->ice_handles);
-					while(g_hash_table_iter_next(&iter, NULL, &value)) {
-						janus_ice_handle *h = value;
-						if(!h || g_atomic_int_get(&stop)) {
-							continue;
-						}
-						janus_ice_handle_destroy(session, h->handle_id);
-						g_hash_table_iter_remove(&iter);
-					}
-				}
-				*／
-				janus_mutex_unlock(&session->mutex);
+
 				/* Notify the transport */
 				if(session->source) {
 					json_t *event = json_object();
@@ -461,6 +442,8 @@ static gboolean janus_check_sessions(gpointer user_data) {
 				if(janus_events_is_enabled())
 					janus_events_notify_handlers(JANUS_EVENT_TYPE_SESSION, session->session_id, "timeout", NULL);
 
+				/* Mark the session as over, we'll deal with it later */
+				session->timeout = 1;
 				/* FIXME Is this safe? apparently it causes hash table errors on the console */
 				g_hash_table_iter_remove(&iter);
 				g_hash_table_insert(old_sessions, janus_uint64_dup(session->session_id), session);
@@ -557,6 +540,21 @@ gint janus_session_destroy(guint64 session_id) {
 		return -1;
 	}
 	JANUS_LOG(LOG_VERB, "Destroying session %"SCNu64"\n", session_id);
+	session->destroy = 1;
+	if (session->ice_handles != NULL && g_hash_table_size(session->ice_handles) > 0) {
+		GHashTableIter iter;
+		gpointer value;
+		/* Remove all handles */
+		g_hash_table_iter_init(&iter, session->ice_handles);
+		while (g_hash_table_iter_next(&iter, NULL, &value)) {
+			janus_ice_handle *handle = value;
+			if(!handle || g_atomic_int_get(&stop)) {
+				continue;
+			}
+			janus_ice_handle_destroy(session, handle->handle_id);
+			g_hash_table_iter_remove(&iter);
+		}
+	}
 
 	/* FIXME Actually destroy session */
 	janus_session_free(session);
@@ -870,27 +868,9 @@ int janus_process_incoming_request(janus_request *request) {
 			ret = janus_process_error(request, session_id, transaction_text, JANUS_ERROR_INVALID_REQUEST_PATH, "Unhandled request '%s' at this path", message_text);
 			goto jsondone;
 		}
-		janus_mutex_lock(&sessions_mutex);
 		/* Schedule the session for deletion */
-		janus_mutex_lock(&session->mutex);
 		session->destroy = 1;
-		/* Remove all handles */
-		/* 
-		if(session->ice_handles != NULL && g_hash_table_size(session->ice_handles) > 0) {
-			GHashTableIter iter;
-			gpointer value;
-			g_hash_table_iter_init(&iter, session->ice_handles);
-			while(g_hash_table_iter_next(&iter, NULL, &value)) {
-				janus_ice_handle *h = value;
-				if(!h || g_atomic_int_get(&stop)) {
-					continue;
-				}
-				janus_ice_handle_destroy(session, h->handle_id);
-				g_hash_table_iter_remove(&iter);
-			}
-		}
-		*／
-		janus_mutex_unlock(&session->mutex);
+		janus_mutex_lock(&sessions_mutex);
 		g_hash_table_remove(sessions, &session->session_id);
 		g_hash_table_insert(old_sessions, janus_uint64_dup(session->session_id), session);
 		GSource *timeout_source = g_timeout_source_new_seconds(3);
